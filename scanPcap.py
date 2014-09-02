51bf44dad33e47debf96c7368c6827b5706d4db0
umbSublime <umbsublime@gmail.com>
2014-09-01 18:51:39 -0700

    wtf

--- scanPcap.py
+++ scanPcap.py
@@ -14,8 +14,8 @@ def setArgs():
     parser = OptionParser(usage = 'usage: %prog [-r FILE] arguments')
     parser.add_option("-r", "--read", dest="filename",
                       help="read pcap FILE", metavar="FILE")
-    parser.add_option("-q", "--quiet",
-                      action="store_false", dest="verbose", default=True,
+    parser.add_option("-v", "--verbose",
+                      action="store_false", dest="verbose", default=False,
                       help="don't print status messages to stdout")
     parser.add_option("-u", "--url",
                       action="store_true", dest="pUrl", default=False,
